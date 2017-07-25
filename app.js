cd812ab2613d332a1847cd5cdcd5f3a1342f73c8
ldevernay <boblehamster@yahoo.fr>
2017-07-25 16:08:31 +0200

    full app

--- app.js
+++ app.js
@@ -4,32 +4,32 @@ const geocode = require('./geocode/geocode');
 const weather = require('./weather/weather');
 
 const argv = yargs
-// .options({
-//   a: {
-//     demand: true,
-//     alias: 'address',
-//     describe: 'Address to fetch weather for',
-//     string: true
-//   }
-// })
-// .help()
-// .alias('help', 'h')
+.options({
+  a: {
+    demand: true,
+    alias: 'address',
+    describe: 'Address to fetch weather for',
+    string: true
+  }
+})
+.help()
+.alias('help', 'h')
 .argv;
 
-// geocode.geocodeAddress(argv.a, (errorMessage, results) => {
-//   if (errorMessage){
-//     console.log(errorMessage);
-//   } else {
-//     console.log(JSON.stringify(results, undefined, 2));
-//   }
-// });
-
-weather.getWeather(43.1337266, 0.7152748999999999, (errorMessage, weatherResults) => {
+geocode.geocodeAddress(argv.a, (errorMessage, results) => {
   if (errorMessage){
     console.log(errorMessage);
   } else {
-    console.log(`
-      Temperature: ${weatherResults.temperature}
-      Apparent temperature: ${weatherResults.apparentTemperature}`);
-  }
-});
+    console.log(`Address: ${results.address}`);
+    weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
+      if (errorMessage){
+        console.log(errorMessage);
+      } else {
+        console.log(`
+          Temperature: ${weatherResults.temperature}
+          Apparent temperature: ${weatherResults.apparentTemperature}`);
+        }
+      });
+
+    }
+  });
