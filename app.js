c87eb832e4b285fc62b9a3cae7413245d0dcf5df
ldevernay <boblehamster@yahoo.fr>
2017-07-25 15:44:10 +0200

    fetch temperature -test

--- app.js
+++ app.js
@@ -1,24 +1,39 @@
-const yargs = require('yargs');
+// const yargs = require('yargs');
+//
+// const geocode = require('./geocode/geocode');
+//
+// const argv = yargs
+// .options({
+//   a: {
+//     demand: true,
+//     alias: 'address',
+//     describe: 'Address to fetch weather for',
+//     string: true
+//   }
+// })
+// .help()
+// .alias('help', 'h')
+// .argv;
+//
+// geocode.geocodeAddress(argv.a, (errorMessage, results) => {
+//   if (errorMessage){
+//     console.log(errorMessage);
+//   } else {
+//     console.log(JSON.stringify(results, undefined, 2));
+//   }
+// });
+//
+// // API key : da18cab2040b7f77c3200b4908a0bca7
 
-const geocode = require('./geocode/geocode');
+const request = require('request');
 
-const argv = yargs
-.options({
-  a: {
-    demand: true,
-    alias: 'address',
-    describe: 'Address to fetch weather for',
-    string: true
-  }
-})
-.help()
-.alias('help', 'h')
-.argv;
-
-geocode.geocodeAddress(argv.a, (errorMessage, results) => {
-  if (errorMessage){
-    console.log(errorMessage);
+request({
+  url: `https://api.forecast.io/forecast/da18cab2040b7f77c3200b4908a0bca7/43.1337266,0.7152748999999999`,
+  json: true
+}, (error, response, body) => {
+  if (!error && response.statusCode === 200){
+    console.log(body.currently.temperature);
   } else {
-    console.log(JSON.stringify(results, undefined, 2));
+    console.log('Unable to fetch weather.');
   }
 });
