17ebab820ed72d0473f72ff693990b12434047c6
ldevernay <boblehamster@yahoo.fr>
2017-07-25 12:01:30 +0200

    address/lat/lng

--- app.js
+++ app.js
@@ -4,5 +4,11 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1+place+marechal+juin+31800+Saint-Gaudens',
   json: true
 }, (error, response, body) => {
-  console.log(body);
+  // console.log(JSON.stringify(body, undefined, 2));
+  let result = body.results[0];
+  console.log(`
+    Address: ${result.formatted_address}
+    Latitude: ${result.geometry.location.lat}
+    Longitude: ${result.geometry.location.lng}
+    `);
 });
