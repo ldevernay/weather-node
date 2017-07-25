7596baf6ec0b471a29a39cd31080b4096e55db41
ldevernay <boblehamster@yahoo.fr>
2017-07-25 13:43:36 +0200

    error handling

--- app.js
+++ app.js
@@ -20,11 +20,17 @@ request({
   url: `https://maps.googleapis.com/maps/api/geocode/json?address= ${encodedAddress}`,
   json: true
 }, (error, response, body) => {
-  // console.log(JSON.stringify(body, undefined, 2));
-  let result = body.results[0];
-  console.log(`
-    Address: ${result.formatted_address}
-    Latitude: ${result.geometry.location.lat}
-    Longitude: ${result.geometry.location.lng}
-    `);
+  if (error) {
+    console.log('Unable to connect to Google servers.');
+  } else if (body.status === 'ZERO_RESULTS'){
+    console.log('Unable to find this address.');
+  } else if (body.status === 'OK') {
+    let result = body.results[0];
+    // console.log(JSON.stringify(, undefined, 2));
+    console.log(`
+      Address: ${result.formatted_address}
+      Latitude: ${result.geometry.location.lat}
+      Longitude: ${result.geometry.location.lng}
+      `);
+    }
   });
